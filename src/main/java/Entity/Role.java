b5880a1b0732982b70e7bb42ce6aae519c227f04
mensia <hana.mensia@esprit.tn>
2023-02-15 20:42:39 +0100

    1.0

---
+++ src/main/java/Entity/Role.java
@@ -0,0 +1,5 @@
+package Entity;
+
+public enum Role {
+    ADMIN , BIOLOGISTE , CHIEF_SERVICE , TRAINER , FORMER , PARTICIPANT , PATIENT
+}
--- src/main/java/Entity/user.java
+++ src/main/java/Entity/user.java
@@ -1,4 +1,35 @@
 package Entity;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
 
-public class user {
+import jakarta.persistence.*;
+import java.io.Serializable;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@ToString
+
+@Entity
+public class user implements Serializable {
+    private static final long haja= 1L;
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    Integer idUser ;
+    String nom ;
+    String prenom ;
+    int phone ;
+    String email ;
+    String password ;
+    Role role ;
+    int salaire ; // pour le formateur et le chef service et les biologiste
+    int work_day_number ; // pour les biologiste
+    int holiday_number ; // pour les biologiste
+    String Social_number ; // pour les patient
+
+
+
 }
