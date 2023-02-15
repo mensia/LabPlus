b57d0799ff391ebd923b196bf071af94c5618b5f
mensia <hana.mensia@esprit.tn>
2023-02-15 18:42:46 +0100

    first commit

---
+++ src/test/java/com/example/labo/LaboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.labo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
